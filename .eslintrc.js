90b7b5e83a2f59c6c857ce5b94cf4c0c35037478
Thanh Phong <phong.huynh@zien.vn>
2023-08-14 16:35:17 +0700

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    'no-var': 'error',
+    'no-multi-spaces': 'error',
+    'no-unused-vars': 'error',
+    'no-trailing-spaces': 'error',
+    'react-native/no-inline-styles': 'error',
+  },
 };
